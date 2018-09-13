3d26a705f0e19ea1bc6325f3269238f429d1535e
Cinnamon18 <alice.ava.moh@gmail.com>
2018-09-13 14:12:04 -0400

    readme fix
    somehow merge conflict markers got into the readme :p

--- Assets/Scripts/Units/MeleeUnit.cs
+++ Assets/Scripts/Units/MeleeUnit.cs
@@ -15,11 +15,11 @@ namespace Units {
 		//attributes specific to Melee
 		private readonly DamageType damageType;
 		private readonly int meleeAttackStrength;
-	
-		public MeleeUnit(ArmorType armorType, int maxHealth, MoveType moveType, int moveDistance, DamageType damageType, int meleeAttackStrength) : base(armorType, maxHealth, moveType,moveDistance) {
-           this.damageType = damageType;
-		   this.meleeAttackStrength = meleeAttackStrength;
-        }
+
+		public MeleeUnit(ArmorType armorType, int maxHealth, MoveType moveType, int moveDistance, DamageType damageType, int meleeAttackStrength) : base(armorType, maxHealth, moveType, moveDistance) {
+			this.damageType = damageType;
+			this.meleeAttackStrength = meleeAttackStrength;
+		}
 
 		public override bool doBattleWith(Unit enemy, Tile enemyTile, Battlefield battlefield) {
 			float damage = this.meleeAttackStrength * (1f * (this as Unit).health / this.maxHealth);
@@ -47,18 +47,14 @@ namespace Units {
 		public override List<Unit> getTargets(int myX, int myY, Battlefield battlefield, Character character) {
 
 			List<Unit> targets = new List<Unit>();
-			List<Coord> tiles = new List<Coord>();
-			tiles.Add(new Coord(myX+1,myY));
-			tiles.Add(new Coord(myX-1,myY));
-			tiles.Add(new Coord(myX,myY+1));
-			tiles.Add(new Coord(myX,myY-1));
+			Coord[] tiles = new Coord[] { new Coord(myX + 1, myY), new Coord(myX - 1, myY), new Coord(myX, myY + 1), new Coord(myX, myY - 1) };
 
 			foreach (Coord tile in tiles) {
-				if(!(tile.x < 0 || tile.y < 0 || tile.x >= battlefield.map.GetLength(0) || tile.y >= battlefield.map.GetLength(1))) {
+				if (!(tile.x < 0 || tile.y < 0 || tile.x >= battlefield.map.GetLength(0) || tile.y >= battlefield.map.GetLength(1))) {
 					Unit targetUnit = battlefield.units[tile.x, tile.y];
-						if(targetUnit != null && targetUnit.getCharacter(battlefield) != character) {
-							targets.Add(targetUnit);
-						}
+					if (targetUnit != null && targetUnit.getCharacter(battlefield) != character) {
+						targets.Add(targetUnit);
+					}
 				}
 			}
 			return targets;
