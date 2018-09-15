199ad41eb05d3283453c2f6196f6a66f44a26d7e
zgoddard3 <zgoddard3@gatech.edu>
2018-09-14 21:21:38 -0400

    Fixed AI attacking units out of range

--- Assets/Scripts/AI/SpecificAgents/simpleAgent.cs
+++ Assets/Scripts/AI/SpecificAgents/simpleAgent.cs
@@ -13,16 +13,22 @@ namespace AI {
 
 		public override async Task<Move> getMove() {
 			Unit unit = selectUnit();
-			Coord unitCoord = battlefield.getUnitCoords(unit);
+			Coord unitCoord = battlefield.getUnitCoords(unit);	
 
 			Coord targetCoord = findNearestEnemy(unitCoord);
+
+			List<Unit> targets = unit.getTargets(unitCoord.x, unitCoord.y, battlefield, character);
+
+			if (targets.Any(t => t == battlefield.units[targetCoord.x, targetCoord.y])) {
+				return new Move(unitCoord.x, unitCoord.y, targetCoord.x, targetCoord.y);
+			}
 
 			int minDist = Int32.MaxValue;
 			Coord bestCoord = unitCoord;
 			foreach (Coord coord in unit.getValidMoves(unitCoord.x, unitCoord.y, battlefield)) {
-				int dist = Math.Abs(targetCoord.x - coord.x) + Math.Abs(targetCoord.y - coord.y);
+				int dist = this.manhattanDistance(coord, targetCoord);
 				IBattlefieldItem item = battlefield.battlefieldItemAt(coord.x, coord.y, 0);
-				if (!(item is Unit && battlefield.charactersUnits[character].Contains(item as Unit)) && dist < minDist) {
+				if (!(item is Unit) && dist < minDist) {
 					minDist = dist;
 					bestCoord = coord;
 				}
@@ -77,6 +83,63 @@ namespace AI {
 				}
 			}
 			return start;
+		}
+
+		private List<Coord> findNearestEnemies(Coord start) {
+			int[,] moveDirs = new int[,] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+			HashSet<Coord> visited = new HashSet<Coord>();
+			Queue<Coord> moveQueue = new Queue<Coord>();
+			moveQueue.Enqueue(start);
+			visited.Add(start);
+
+			List<Coord> enemies = new List<Coord> ();
+			int minDist = Int32.MaxValue;
+
+			while (moveQueue.Count() > 0) {
+				Coord curCoord = moveQueue.Dequeue();
+
+				if (manhattanDistance(start, curCoord) > minDist) {
+					return enemies;
+				}
+
+				IBattlefieldItem item = battlefield.battlefieldItemAt(curCoord.x, curCoord.y, 0);
+				if (item is Unit) {
+					Unit unit = item as Unit;
+					if (!battlefield.charactersUnits[character].Contains(unit)) {
+						enemies.Add(curCoord);
+						if (minDist == Int32.MaxValue) {
+							minDist = manhattanDistance(start, curCoord);
+						}
+					}
+				}
+
+				for (int x = 0; x < moveDirs.GetLength(0); x++) {
+					Coord nextCoord = new Coord(curCoord.x + moveDirs[x, 0], curCoord.y + moveDirs[x, 1]);
+					if (nextCoord.x >= 0 && nextCoord.y >= 0 && nextCoord.x < battlefield.map.GetLength(0) && nextCoord.y < battlefield.map.GetLength(1)) {
+						if (!visited.Contains(nextCoord)) {
+							visited.Add(nextCoord);
+							moveQueue.Enqueue(nextCoord);
+						}
+					}
+				}
+			}
+			return enemies;
+		}
+
+		private List<Coord> findAllEnemies() {
+			List<Coord> enemies = new List<Coord>();
+			for (int x = 0; x < battlefield.units.GetLength(0); x++) {
+				for (int y = 0; y < battlefield.units.GetLength(1); y++) {
+					if (battlefield.units[x,y] != null && battlefield.units[x,y].getCharacter(battlefield) != character) {
+						enemies.Add(new Coord(x,y));
+					}
+				}
+			}
+			return enemies;
+		}
+
+		private int manhattanDistance(Coord coord1, Coord coord2) {
+			return Math.Abs(coord2.x - coord1.x) + Math.Abs(coord2.y - coord1.y);
 		}
 	}
 }
