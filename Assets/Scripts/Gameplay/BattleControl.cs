ac74746a18ee060453234e0e099a0c41d5c3bdad
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-09-22 18:03:03 -0400

    Added level deserializition to DemoBattle and another level deserialization in TestLevelEditor
    Can test created levels in TestLevelEditor.  Just put name of level in LevelName attached to BattleControl object.

--- Assets/Scripts/Gameplay/BattleControl.cs
+++ Assets/Scripts/Gameplay/BattleControl.cs
@@ -10,6 +10,8 @@ using Cutscenes;
 using System.Threading.Tasks;
 using AI;
 using Buffs;
+using System.IO;
+using Editors;
 
 namespace Gameplay {
 	public class BattleControl : MonoBehaviour {
@@ -109,14 +111,38 @@ namespace Gameplay {
 			switch (battleStage) {
 				case BattleLoopStage.Initial:
 					if (!cutscene.inProgress) {
+						//Testing Level Deserialization
+						LevelInfo levelInfo = Serialization.getLevel("testdemo");
+						try
+						{
+							Stack<UnitInfo> stack = levelInfo.units;
+							while (stack.Count != 0)
+							{
+								UnitInfo info = stack.Pop();
+								if (info.getIsPlayer())
+								{
+									addUnit(info.getUnitType(), level.characters[0], info.getCoord().x, info.getCoord().y, Faction.Xingata);
+								}
+								else
+								{
+									addUnit(info.getUnitType(), level.characters[1], info.getCoord().x, info.getCoord().y, Faction.Tsubin);
+								}
 
+							}
+						}
+						catch (FileNotFoundException ex)
+						{
+							Debug.Log("Incorrect level name" + ex.ToString());
+						}
+						
+						/*
 						//TODO This is temp just for testing until level editor deserialization. 
 						addUnit(UnitType.Knight, level.characters[0], 0, 0, Faction.Xingata);
 						addUnit(UnitType.Knight, level.characters[0], 1, 0, Faction.Xingata);
 						addUnit(UnitType.Knight, level.characters[0], 0, 1, Faction.Xingata);
 						addUnit(UnitType.Knight, level.characters[1], 3, 7, Faction.Tsubin);
 						addUnit(UnitType.Knight, level.characters[1], 4, 7, Faction.Tsubin);
-
+						*/
 						advanceBattleStage();
 					}
 					break;
