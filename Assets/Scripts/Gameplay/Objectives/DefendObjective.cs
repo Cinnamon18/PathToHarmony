9117f64755aff411b245581e3510f6bdb6679362
zgoddard3 <zgoddard3@gatech.edu>
2018-09-21 23:50:31 -0400

    Added more objectives

--- Assets/Scripts/Gameplay/Objectives/DefendObjective.cs
+++ Assets/Scripts/Gameplay/Objectives/DefendObjective.cs
@@ -2,17 +2,17 @@
 using Units;
 
 namespace Gameplay {
-	public class CaptureObjective : GameObjective {
+	public class DefendObjective : GameObjective {
 
 		public List<Coord> capturePoints = new List<Coord>();
 		public int timeToHold = 0;
 		private int timeHeld = 0;
 		private int lastHalfTurnsElapsed = 0;
 
-		public CaptureObjective(Battlefield battlefield, Level level, Character playerCharacter, int maxHalfTurns) :
+		public DefendObjective(Battlefield battlefield, Level level, Character playerCharacter, int maxHalfTurns) :
 			base(battlefield, level, playerCharacter, maxHalfTurns) { }
 
-		public CaptureObjective(Battlefield battlefield, Level level, Character playerCharacter, int maxHalfTurns, List<Coord> capturePoints, int timeToHold) :
+		public DefendObjective(Battlefield battlefield, Level level, Character playerCharacter, int maxHalfTurns, List<Coord> capturePoints, int timeToHold) :
 			base(battlefield, level, playerCharacter, maxHalfTurns) {
 				this.capturePoints = capturePoints;
 				this.timeToHold = timeToHold;
@@ -21,16 +21,10 @@ namespace Gameplay {
 		public override bool isLoseCondition(int halfTurnsElapsed) {
 			if (battlefield.charactersUnits[playerCharacter].Count == 0) {
 				return true;
-			} else if (halfTurnsElapsed >= maxHalfTurns) {
-				return true;
 			}
-			return false;
-		}
-
-		public override bool isWinCondition(int halfTurnsElapsed) {
 			foreach (Coord coord in this.capturePoints) {
 				Unit unit = battlefield.units[coord.x, coord.y];
-				if (unit == null || unit.getCharacter(battlefield) != playerCharacter) {
+				if (unit == null || unit.getCharacter(battlefield) == playerCharacter) {
 					timeHeld = 0;
 					return false;
 				}
@@ -40,6 +34,20 @@ namespace Gameplay {
 				lastHalfTurnsElapsed = halfTurnsElapsed;
 			}
 			return timeHeld >= timeToHold;
+		}
+
+		public override bool isWinCondition(int halfTurnsElapsed) {
+			if (halfTurnsElapsed >= maxHalfTurns) {
+				return true;
+			}
+			foreach (Character character in battlefield.charactersUnits.Keys) {
+				if (character != playerCharacter) {
+					if (battlefield.charactersUnits[character].Count != 0) {
+						return false;
+					}
+				}
+			}
+			return true;
 		}
 	}
 }
---
+++ Assets/Scripts/Gameplay/Objectives/EscortObjective.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Units;
+
+namespace Gameplay {
+	public class EscortObjective : GameObjective {
+
+		public List<Unit> vips = new List<Unit>();
+		private int lastHalfTurnsElapsed = 0;
+
+		public EscortObjective(Battlefield battlefield, Level level, Character playerCharacter, int maxHalfTurns) :
+			base(battlefield, level, playerCharacter, maxHalfTurns) { }
+
+		public EscortObjective(Battlefield battlefield, Level level, Character playerCharacter, int maxHalfTurns, List<Unit> vips) :
+			base(battlefield, level, playerCharacter, maxHalfTurns) {
+				this.vips = vips;
+			 }
+
+		public override bool isLoseCondition(int halfTurnsElapsed) {
+			if (battlefield.charactersUnits[playerCharacter].Count == 0) {
+				return true;
+			}
+			foreach (Unit unit in this.vips) {
+				if (!battlefield.charactersUnits[playerCharacter].Contains(unit)) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public override bool isWinCondition(int halfTurnsElapsed) {
+			if (halfTurnsElapsed >= maxHalfTurns) {
+				return true;
+			}
+			foreach (Character character in battlefield.charactersUnits.Keys) {
+				if (character != playerCharacter) {
+					if (battlefield.charactersUnits[character].Count != 0) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
