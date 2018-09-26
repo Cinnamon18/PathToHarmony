e897eea3be81142e3777e24c4f64ed4de1df21cd
Grace Rarer <gracerarer@gmail.com>
2018-09-26 11:05:40 -0400

    StatusUnit
    StatusUnit is finished, but for now Mage is using the DamageBuff and the knight's model and textures as placeholders.

--- Assets/Scripts/Gameplay/BattleControl.cs
+++ Assets/Scripts/Gameplay/BattleControl.cs
@@ -120,6 +120,8 @@ namespace Gameplay {
 						addUnit(UnitType.Knight, level.characters[1], 3, 7, Faction.Tsubin);
 						addUnit(UnitType.Knight, level.characters[1], 4, 7, Faction.Tsubin);
 
+						addUnit(UnitType.Mage, level.characters[0], 1, 1, Faction.Xingata);
+
 						// Uncomment these for the escort objective
 						// (objective as EscortObjective).vips.Add(battlefield.units[0,0]);
 						// (objective as EscortObjective).vips.Add(battlefield.units[1,0]);
