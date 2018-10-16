9e2a1dc11cabc5d21e2886373863cb584adf3c47
Matthew M <mattmusselman01@gmail.com>
2018-10-15 21:14:50 -0400

    Re-added end turn button

--- Assets/Scripts/UI/EndTurnButton.cs
+++ Assets/Scripts/UI/EndTurnButton.cs
@@ -9,17 +9,14 @@ public class EndTurnButton : MonoBehaviour {
 	public BattleControl control;
 	public Button self;
 	public Image image;
-	public Text text;
 	// Update is called once per frame
 	void Update () {
 		if (control.PlayerCharacter == control.CurrentCharacter) {
 			self.interactable = true;
 			image.color = new Color(image.color.r, image.color.g, image.color.b,Mathf.Min(1, image.color.a + 1 * Time.deltaTime));
-			text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Min(1, text.color.a + 1 * Time.deltaTime));
 		} else {
 			self.interactable = false;
 			image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Max(0.25f, image.color.a - 1 * Time.deltaTime));
-			text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Max(0.25f, text.color.a - 1 * Time.deltaTime));
 		}
 	}
 }
