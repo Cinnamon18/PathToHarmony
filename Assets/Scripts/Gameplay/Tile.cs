c5a310b532458d42864484a4bc6eead3651d1be7
Andrew Ho <aho3555@gmail.com>
2018-10-18 05:24:16 -0400

    Added tile effects

--- Assets/Scripts/Gameplay/Tile.cs
+++ Assets/Scripts/Gameplay/Tile.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using Constants;
 using System;
 using Gameplay;
-using Units;
 
 namespace Gameplay {
 	public class Tile : MonoBehaviour, IBattlefieldItem {
@@ -12,11 +11,9 @@ namespace Gameplay {
 
 		[SerializeField]
 		private TileType initialType;
-        [SerializeField]
-        private TileEffects Effect;
 		public TileType tileType { get; set; }
 		public static GameObject[][] tileFlavor;
-
+		public TileEffects tileEffects;
 		public Tile() : this(TileType.None) { }
 
 		public Tile(TileType tileType) {
@@ -64,29 +61,5 @@ namespace Gameplay {
 		public override string ToString() {
 			return tileType.ToString();
 		}
-
-        public void OnCollisionEnter(Collision col)
-        {
-            Unit unit = col.gameObject.GetComponent<Unit>();
-            Debug.Log(ToString());
-            switch (Effect)
-            {
-                case TileEffects.Normal:
-                    return;
-                    break;
-                case TileEffects.DOT:
-                    if (col.gameObject is Unit && unit != null)
-                    {
-                        unit.setHealth(unit.getHealth() - 30);
-                    }
-                    break;
-                case TileEffects.Heal:
-                    if (col.gameObject is Unit && unit != null)
-                    {
-                        unit.setHealth(unit.getHealth() + 5);
-                    }
-                    break;
-            }
-        }
 	}
 }
