006a3a36092c3c804dc934fb2564da82723b200a
Grace Rarer <gracerarer@gmail.com>
2018-10-24 20:34:41 -0400

    Revert "Level progress persistence"
    This reverts commit 0232727f91ee2ab3cd15ca57a017463ef5607f74.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -97,9 +97,6 @@ public class MainMenu : MonoBehaviour {
 
 		Persistance.campaign = testCampaign1;
 
-		//load the player's current progress
-		Persistance.campaign.levelIndex=Persistance.getLevelIndex();
-
 
 	}
 }
