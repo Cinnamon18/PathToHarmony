81dc77f2f279f3c6e0a1d298ccb4abefd0003677
Cinnamon18 <alice.ava.moh@gmail.com>
2018-10-26 16:44:26 -0400

    added rogue and light horsey units

---
+++ Assets/Scripts/Units/LightHorse.cs
@@ -0,0 +1,9 @@
+using Constants;
+
+namespace Units {
+	public class LightHorse : MeleeUnit {
+		public LightHorse() : base(ArmorType.Light, 100, MoveType.ArmoredInfantry, 6, DamageType.Slash, 50, Faction.Xingata) {
+
+		}
+	}
+}
